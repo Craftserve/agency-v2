a143261ef4d18c2f65c5e597092ab712cca92cdc
rozwader <maciek.rozwadowski2006@gmail.com>
2025-08-22 14:12:32 +0200

    feature: i18n configuration

---
+++ src/i18n/ui.ts
@@ -0,0 +1,10 @@
+export const defaultLang = "pl-PL";
+
+export const ui = {
+    "en": {
+        "example": "test",
+    },
+    "pl-PL": {
+        "example": "test po polsku"
+    }
+} as const;
---
+++ src/i18n/utils.ts
@@ -0,0 +1,18 @@
+import { ui, defaultLang } from './ui';
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
+
+export function getLangForRedirect(url: URL) {
+    const [, lang] = url.pathname.split('/');
+    if(lang in ui) return lang as keyof typeof ui;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof typeof ui[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  }
+}
