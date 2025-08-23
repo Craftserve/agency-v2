ace3c8491816c93e308caaf2cf8f50c0af9381dd
rozwader <maciek.rozwadowski2006@gmail.com>
2025-08-23 14:34:11 +0200

    feature: finished hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
     output: "static",
+    integrations: [icon({
+        include: {
+            uil: ["*"],
+        }
+    })],
 });
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,10 +5,16 @@ export const ui = {
         "example": "test",
         "about": "About",
         "contact": "Contact",
+        "joinus": "Join us",
+        "learnmore": "learn more",
+        "aboutus": "About us",
     },
     "pl-PL": {
         "example": "test po polsku",
         "about": "Dlaczego my",
         "contact": "Kontakt",
+        "joinus": "Dołącz do nas",
+        "learnmore": "dowiedz się",
+        "aboutus": "Więcej o nas",
     }
 } as const;
