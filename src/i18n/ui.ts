af0c0464727408bae03e62c36d9aceda3b48c281
rozwader <maciek.rozwadowski2006@gmail.com>
2025-09-08 19:59:05 +0200

    feature: added translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -12,6 +12,18 @@ export const ui = {
 
         "aboutUrl": "about",
         "contactUrl": "#formSection",
+        
+        "name": "Name",
+        "surname": "Surname",
+        "phone": "123 456 789",
+        "subject": "Subject",
+        "email": "E-mail",
+        "company": "Company",
+        "content": "Content",
+        "send": "Send",
+
+        "formTitle": "Achieve Your Business Goals with Vitresoft",
+        "formDesc": "You will receive a response at the provided email address within 24 hours. We will answer all your questions and propose solutions tailored to your budget."
     },
     "pl-PL": {
         "example": "test po polsku",
@@ -24,5 +36,16 @@ export const ui = {
 
         "aboutUrl": "o-nas",
         "contactUrl": "#formSection",
+
+        "name": "Imię",
+        "surname": "Nazwisko",
+        "phone": "123 456 789",
+        "subject": "Temat",
+        "email": "E-mail",
+        "company": "Firma",
+        "content": "Treść",
+        "send": "Wyślij",
+        "formTitle": "Ukończ cele swojej firmy z Vitresoft",
+        "formDesc": "Otrzymasz odpowiedź na podany adres email w przeciągu 24 godzin. Odpowiemy na wszystkie nurtujące pytania i zaproponujemy rozwiązania do przedstawionego budżetu"
     }
 } as const;
