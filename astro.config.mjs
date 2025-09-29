89e82ac77791c874de7225d5e3928974de7ec7d9
rozwader <maciek.rozwadowski2006@gmail.com>
2025-09-29 15:59:16 +0200

    chore: prepared site for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
+const LIVE_URL = "https://Craftserve.github.io"
+
 // https://astro.build/config
 export default defineConfig({
+    site: LIVE_URL,
     output: "static",
     integrations: [
         icon({
