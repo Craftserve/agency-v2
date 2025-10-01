2d190b4967617d6a4c46011e355bb5f743272c8d
rozwader <maciek.rozwadowski2006@gmail.com>
2025-10-01 21:02:27 +0200

    chore: fixed validator.w3 error

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,6 @@ const LIVE_URL = "https://Craftserve.github.io"
 export default defineConfig({
     site: LIVE_URL,
     output: "static",
-    site: "https://Craftserve.github.io",
-    base: "agency-v2",
     integrations: [
         icon({
             include: {
